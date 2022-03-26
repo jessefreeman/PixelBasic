f11d084a6e6dfb85b8912f754729b91c1b3ed255
Jesse Freeman <jessefreeman@gmail.com>
2022-03-26 13:48:45 -0400

    Inital Import.

---
+++ Interpreter.cs
@@ -0,0 +1,25 @@
+namespace PixelBasic;
+
+public class Interpreter
+{
+    private bool _running = false;
+
+    public Interpreter(string program)
+    {
+        // TODO: Implement
+    }
+
+    public void ExecuteProgram()
+    {
+        _running = true;
+
+        // while (_running && !Finished)
+        // {
+        //     if (_statements.TryGetValue(_currentLineNumber, out var statement))
+        //     {
+        //         statement.Execute(this);
+        //     }
+        // }
+    }
+    
+}
