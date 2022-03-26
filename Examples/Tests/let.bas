5f8c7c19389cc840f37bd9a9414c5c823a4daae8
Jesse Freeman <jessefreeman@gmail.com>
2022-03-26 19:38:21 -0400

    Cleaning up tests.

--- Examples/Tests/let.bas
+++ Examples/Tests/let.bas
@@ -1,7 +1,3 @@
 LET string = "foo"
 LET numeric = 123
 LET nan = VAL("Not A Number")
-
-PRINT string
-PRINT numeric
-PRINT nan
--- Program.cs
+++ Program.cs
@@ -10,14 +10,40 @@ namespace PixelBasic
         {
             try
             {
-                if (args.Length != 1)
+                
+                // For debugging
+                args = new[]
+                {
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/for_continue.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/for_exit.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/for_simple.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/functions.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/gcd_euclid.bas", // TODO Not working
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/if_else.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/if_simple_false.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/if_simple_true.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/input.bas", // TODO not working
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/let.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/operations.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/print.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/repeat.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/syntax_err.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/type_err.bas",
+                    "/Users/jessefreeman/Projects/DotNet/PixelBasic/PixelBasic/Examples/Tests/while.bas"
+                };
+                
+                if (args.Length == 0)
                 {
                     Console.WriteLine("Usage PixelBasicApp <programFile>");
                     return;
                 }
 
-                var program = File.ReadAllText(args[0]);
+                var path = args[15];
 
+                var program = File.ReadAllText(path);
+
+                Console.WriteLine("Running program: " + path);
+                
                 var programStream = new MemoryStream(Encoding.UTF8.GetBytes(program));
             
                 var interpreter = new Interpreter(programStream);
